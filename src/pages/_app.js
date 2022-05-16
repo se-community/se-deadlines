b02c4e93b0a9006aaec8834daf4aac5f75dcf8b2
Thiago do N. Ferreira <thiagodonferreira@gmail.com>
2022-05-16 11:24:56 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 // automatically since it's being imported above
 config.autoAddCss = false;
 
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
